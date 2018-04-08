7cac9beb7a56a6bc3d0859aab7878cdc302389ca
machester <shusheng1991@gmail.com>
2018-04-09 00:32:06 +0800

    -LINKDED LIST

--- Liner_Links.c
+++ Liner_Links.c
@@ -1,122 +1,151 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum
-{
+
+typedef enum {
     False = 0,
     True = !False
 }status;
 
-typedef struct{
+struct student{
     int ID;
     char name[20];
     double score;
+
 }students;
 
-//liner linked list
-struct NodeList{
-    students *NodeData;
-    struct Node *nextNode;
-}linkedList;
+typedef struct NodeDef{
+    struct student item;
+    //int item;
+    struct Node *next;
+}Node;
 
-//function statement define
-void SantexInput(students *pStdList);
-void DataCheck(students *pStdList);
-status _F_Quit(void);
-linkedList LinkListCreat(linkedList *list);
-linkedList LinkListInsert(linkedList *list, int number);
+Node *nodeList;
 
+void CreatList(Node **nodeHeard, int length);
+void ShowList(Node *pList);
+void ClearList(Node **pList);
+void InsertList(Node **pList, int i);
 
-
-int parts;
-
-int main(void)
+void InsertList(Node **pList, int i)
 {
-    int selection = 0;
-    parts = 0;
-    status breakFlag = False;
-    students *pStdList;
+    struct student *st;
+}
 
+void CreatList(Node **nodeHeard, int length)
+{
+    Node *p;
+    Node *t;
+    int i;
+
+    *nodeHeard = (Node *)malloc(sizeof(Node));      //init list heard
+    t = *nodeHeard;
+    if(NULL == t)
+        return;
+    printf("Inpput format: ID\t name\t score\t\n");
+    for(i = 0; i < length; i++)
+    {
+        p = (Node *)malloc(sizeof(Node));
+
+        scanf("%d", &(p ->item.ID));
+        scanf("%s", &(p ->item.name));
+        scanf("%lf", &(p ->item.score));
+
+        t ->next = p;
+        t = p;
+    }
+    t ->next = NULL;            //list end
+}
+void ShowList(Node *pList)
+{
+   int i;
+   Node *p;
+
+   p = pList ->next;        //get heard address
+   i = 1;
+   if(NULL == p)
+       printf("list is empty.\n");
+   while (NULL != p)        // list not empty.
+   {
+       printf("%d:  ID: %d,     name: %s,       score: %5.2lf\n",\
+       i, p->item.ID, p ->item.name, p ->item.score);
+
+       p = p ->next;
+       ++i;
+   }
+}
+void ClearList(Node **pList)
+{
+    Node *temp, *q;
+    temp = *pList;
+
+    if(NULL == temp->next)              //heard is empty.
+        printf("list has been clearned.");
+    else
+    {
+        while (NULL != temp)            //heard nont empty
+        {
+            q = temp ->next;
+            free(temp);
+            temp = NULL;
+            temp = q;
+        }
+        // set heard to empty
+        temp = *pList;
+        temp ->next = NULL;
+    }
+
+}
+
+int main()
+{
+
+    int selection = 0;
+    int parts = 0;
+    int length;
+    status breakFlag = False;
     printf("Plese select:\n");
     while(1)
     {
-        printf("1.Input\t 2.Check\t 3.Quit\n");
-        printf("4.Delete Data\t 5.Serach Data\t 6.SoratData\t 7.ChangeData\n");
+        printf("1.Input:    2.Show List:   3.Search:        4.Insert\n");
+        printf("5.Insert:   6.Sort         7.Delete List    8.Quit\n");
 
         scanf("%d", &selection);
-        switch(selection)
-        {
+        switch (selection) {
             case 1:
                 printf("input parts:");
                 scanf("%d", &parts);
-                pStdList = (students *)malloc \
-					     (sizeof(students) * parts);
-                if(NULL == pStdList)
-                {
-                    exit(1);
-                }
-                SantexInput(pStdList);
+                CreatList(&nodeList, parts);
                 break;
             case 2:
-                DataCheck(pStdList);
+                ShowList(nodeList);
                 break;
             case 3:
-                breakFlag = _F_Quit();
+                printf("Reserved\n");
                 break;
             case 4:
+
+                InsertList(&nodeList, 0);
+                printf("Reserved\n");
                 break;
             case 5:
+                printf("Reserved\n");
                 break;
             case 6:
+                printf("Reserved\n");
                 break;
             case 7:
+                ClearList(&nodeList);
                 break;
+            case 8:
+                exit(0);
             default:
-                printf("\n---- input error. -----\n");
+                printf("\n/---- Select error. -----/\n");
                 break;
         }
-        if(True == breakFlag)
+        if (True == breakFlag)
             break;
     }
-    free(pStdList);
-    printf("\n------------- EXTI SUCCESS ---------------\n");
-    return 0;
 }
 
-
-void SantexInput(students *pStdList)
-{
-    int i;
-
-    printf("Input format: \nID\t name\t score\n");
-    for(i = 0; i < parts; i++)
-    {
-        scanf("%d", &((pStdList + i)->ID));
-        scanf("%s", &((pStdList + i)->name));
-        scanf("%lf", &((pStdList + i)->score));
-    }
-    printf("\n---- Input Accepted. ----\n");
-}
-void DataCheck(students *pStdList)
-{
-    int i;
-
-    if(0 == parts)
-        printf("\nDatabase is empty.\n");
-    else
-    {
-        printf("Data List:\n");
-        for(i = 0; i < parts; i++)
-        {
-            printf("%d. ID: %d\t name:%s\t score: %4.2f\n", (i+1), \
-		    (pStdList + i) ->ID, \
-		    (pStdList + i) ->name, \
-		    (pStdList + i) ->score);
-        }
-        printf("\n----- Data check completed. ----\n");
-    }
-}
-status _F_Quit(void)
-{
-    return True;
-}
